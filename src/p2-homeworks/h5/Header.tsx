a4813963676f411da6ce1e5c949754ce7081f531
Astaroth <minatosavich@gmail.com>
2021-06-22 22:46:12 +0300

    feat: add deploy and router

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+import s from "./Header.module.css"
 
-function Header() {
+export function Header() {
+
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.wrapperList}>
+            <ul className={s.listLevel}>
+                <li className={s.listItem}><NavLink to={"./pre-junior"} activeClassName={s.active}>pre-junior </NavLink></li>
+                <li className={s.listItem}><NavLink to={"./junior"} activeClassName={s.active}>junior </NavLink></li>
+                <li className={s.listItem}><NavLink to={"./junior-plus"} activeClassName={s.active}>junior+ </NavLink></li>
+                <li className={s.listItem}><span className={s.menu}>Level Task</span></li>
+            </ul>
         </div>
     )
 }
-
-export default Header
